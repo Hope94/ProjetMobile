d0eb2923a95a9560aad6a242fc44fc2363271381
nesrine94 <dn_arhab@esi.dz>
2017-04-17 23:22:37 +0100

    fixing image view pager

--- app/src/main/java/com/example/nesrine/projetmobile/DetailLog.java
+++ app/src/main/java/com/example/nesrine/projetmobile/DetailLog.java
@@ -5,6 +5,8 @@ import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -16,9 +18,17 @@ public class DetailLog extends AppCompatActivity {
         setContentView(R.layout.activity_detail_log);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        //afficher le bouton retour
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.inflateMenu(R.menu.toolbar_menu);
+
         CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
         collapsingToolbarLayout.setTitle(" ");
-       // Button btn_map= (Button) findViewById(R.id.map_btn);
+
+        // Button btn_map= (Button) findViewById(R.id.map_btn);
 
 
     }
@@ -27,5 +37,25 @@ public class DetailLog extends AppCompatActivity {
         Intent intent=new Intent(DetailLog.this,map_logement.class);
         startActivity(intent);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 
 }
